e8a5258360eec678cb537d0c2b36d6a170c16178
burnningDark <1174072083@qq.com>
2019-01-17 22:51:02 +0800

    完善了chart原型方法，利用继承增强了代码的通用性

--- utils/chart.js
+++ utils/chart.js
@@ -8,31 +8,48 @@ import 'echarts/lib/component/dataset'
 import {
     setExtend
 } from "./extend";
-chart.prototype.settings = function (arg) {
-    //设置常用的几个属性
-
-}
-
-function chart() {
-
-}
-chart.prototype.draw = function (options) {
-    this.chart.setOption(options || this.options);
-};
-chart.prototype.importData = function (columns, rows) {
-    this.options.dataset.dimensions = columns;
-    this.options.dataset.source = rows
-};
-export function barChart(el, width, height) {
-    chart.call(this);
+//chart是父构造函数，包含一些基本的属性，通用的方法
+function chart(el, width, height) {
     this.el = document.getElementById(el);
     this.width = width || "400px";
     this.height = height || "600px";
     this.el.style.width = this.width;
     this.el.style.height = this.height;
     this.chart = echarts.init(this.el);
-
-    this.data = {};
+    this.options = {}
+}
+chart.prototype.draw = function () {
+    this.chart.setOption(this.options);
+};
+chart.prototype.importData = function (columns, rows) {
+    this.options.dataset.dimensions = columns;
+    this.options.dataset.source = rows;
+    var _this = this;
+    columns.forEach(function (item, index) {
+        if (index) {
+            _this.options.series.push({
+                type: _this.type,
+                name: item
+            })
+        }
+    })
+};
+barChart.prototype = Object.create(chart.prototype)
+barChart.prototype.constructor = barChart;
+//继承:通过 new 一个父构造函数,可以产生一个实例对象，且该对象的__proto__指向构造函数的原型
+//如果将此实例对象赋值给子构造函数的原型对象，那么子构造函数就具有了指向父构造函数原型的指针
+//就可以顺着原型链实现继承
+//但是 new 操作符与call函数会造成两个属性的复制
+//因而可以将新建一个空的 var fn=function(){} 并将其 prototype属性指向父构造函数的prototype
+//fn.prototype=parent.prototype 然后再让 child.prototype=new fn();
+//那么child 的原型会指向 fn的原型，而 fn 的原型指向 parent 的原型
+//以上所述，可以用Object.create()来实现....
+chart.prototype.extend = function (options) {
+    setExtend(this.options, options);
+};
+export function barChart(el, width, height) {
+    chart.call(this, el, width, height);
+    this.type = "bar"
     this.options = {
         title: {
             text: "ECharts 入门示例"
@@ -47,25 +64,6 @@ export function barChart(el, width, height) {
         },
         legend: {},
         yAxis: {},
-        series: [{
-                type: 'bar',
-                itemStyle: {
-                    color: 'yellow'
-                }
-
-            },
-            {
-                type: 'bar',
-                itemStyle: {
-                    color: '#456789'
-                }
-            }
-        ]
+        series: []
     };
 }
-barChart.prototype = new chart();
-barChart.prototype.constructor = barChart;
-
-barChart.prototype.extend = function (options) {
-    setExtend(this.options, options);
-};
